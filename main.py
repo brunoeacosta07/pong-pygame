a768ce30e2afb6d09252aee5436db6e21b6a6992
Bruno <brunoeacosta07@gmail.com>
2024-10-17 22:46:02 -0300

    fix: apikey tests removed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import google.generativeai as gemini
 def chat_gpt_completion():
     client = OpenAI(
         api_key=
-        "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"
+        ""
     )
 
     chat_completion = client.chat.completions.create(
@@ -52,7 +52,7 @@ def analyze_sentiment(text_to_analyse):
 
 
 def gemini_generative_text(player, cpu):
-    gemini.configure(api_key='AIzaSyCJY0k_toVwRUSoAWgi4pL3tRrxAY9v48c')
+    gemini.configure(api_key='')
     model = gemini.GenerativeModel('gemini-1.5-flash')
     response = model.generate_content("Teniendo en cuenta la siguiente puntuacion y como si se siguiera jugando," +
                                       " genera un muy breve texto de aliento o felicitaciones " +
